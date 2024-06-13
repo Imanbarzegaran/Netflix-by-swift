5667d8a7527b1a9bb1734d344c8cc010efe3817f
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-06-13 17:21:52 +0330

    start project

---
+++ Netflix project/Netflix project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix project
+//
+//  Created by MacBook Air on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
