5e9dccda29094e7a62c7b3f52ac2fbde4c9a6aca
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-06-21 13:18:41 +0330

    set the upcomng tab by title and poster of movies

---
+++ Netflix project/Netflix project/ViewModels/TitleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleViewModel.swift
+//  Netflix project
+//
+//  Created by MacBook Air on 6/21/24.
+//
+
+import Foundation
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
