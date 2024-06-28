4eb63dc1a45bb97f7bc2f62717dcb61538ea0d53
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-06-28 14:04:30 +0330

    use core data for save the downloaded movie in database

--- Netflix project/Netflix project/Views/CollectionViewTableViewCell.swift
+++ Netflix project/Netflix project/Views/CollectionViewTableViewCell.swift
@@ -56,7 +56,17 @@ class CollectionViewTableViewCell: UITableViewCell {
     }
     
     private func downloadTitleAt(indexPath: IndexPath) {
-        print("Downloding \(titles[indexPath.row].original_title ?? "") ")
+        
+        
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+           
+            switch result {
+            case .success():
+               print("download to Database")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 }
@@ -122,6 +132,8 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
             }
        return config
     }
+    
+    
     
     
 }
